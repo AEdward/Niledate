631e71f1c50352d69a0165fdee094fc17ebcdac0
Anahom Edward <Eanahom@yahoo.com>
2026-05-28 11:30:12 +0300

    Update index.js

--- apps/bot/src/index.js
+++ apps/bot/src/index.js
@@ -2,6 +2,13 @@ import 'dotenv/config'
 import { Telegraf, Markup } from 'telegraf'
 import { welcomeMessage, helpMessage } from './messages.js'
 
+import {
+  startProfileFlow,
+  hasActiveProfileFlow,
+  handleProfileText,
+  handleProfilePhoto
+} from './profile-flow.js'
+
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.start((ctx) => {
@@ -16,8 +23,28 @@ bot.start((ctx) => {
   )
 })
 
+bot.hears('👤 Create Profile', (ctx) => {
+  startProfileFlow(ctx)
+})
+
 bot.hears('❓ Help', (ctx) => {
   ctx.reply(helpMessage)
+})
+
+bot.on('text', async (ctx, next) => {
+  if (hasActiveProfileFlow(ctx)) {
+    const handled = await handleProfileText(ctx)
+    if (handled) return
+  }
+
+  return next()
+})
+
+bot.on('photo', async (ctx, next) => {
+  const handled = await handleProfilePhoto(ctx)
+  if (handled) return
+
+  return next()
 })
 
 bot.launch()
